4c6543e4fa6a6bd07cc0c0f2dc79bea1a9dd5bd6
Liam Arzola <lma77@cornell.edu>
2020-09-26 12:16:38 -0400

    user registration

--- models/users.py
+++ models/users.py
@@ -1,11 +1,67 @@
+import bcrypt
 from pydantic import BaseModel, EmailStr
-
+from datetime import datetime
+from enum import Enum, auto
+from typing import Optional
+from starlette.exceptions import HTTPException
 
 class register_form_output(BaseModel):
     success: bool
 
+class AutoName(Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+class TransportEnum(AutoName):
+    CAR = auto()
+    BIKE = auto()
+    FOOT = auto()
+
+class StatusEnum(AutoName):
+    CREATED = auto()
+    ACCEPTED = auto()
+    PICKED_UP = auto()
+    INVALID = auto()
+    COMPLETED = auto()
 
 class register_form_input(BaseModel):
     email: EmailStr
-    first_name: str
-    last_name: str
+    password: str
+    first: str
+    last: str
+    age: int
+    address: str
+    volunteer: bool
+    transport: TransportEnum
+
+class FullUserData(register_form_input):
+    _id: str
+    points: int
+    trips: int
+    hours: float
+
+    def change_password(self, new_password: str):
+        try:
+            assert len(new_password) >= 8
+        except:
+            raise HTTPException(
+                    status_code=422,
+                    detail="Password does not meet minimum length requirement (length >= 10)"
+                    )
+        self.password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+
+    def check_password(self, pwd: str) -> bool:
+        match = bcrypt.checkpw(pwd.encode('utf-8'), self.password.encode('utf-8'))
+        return match
+
+
+class ticket_form_input(BaseModel):
+    created: datetime
+    destinationAddress: str
+    orderNumber: str
+    author: str
+    status: StatusEnum
+    volunteer: Optional[str]
+    contactInfo: str
+    expireAt: datetime
+
--- util/users.py
+++ util/users.py
@@ -1,7 +1,14 @@
 import models.users as models
-
+import bcrypt
+import uuid
 
 async def register_user(form, db):
     column = db["carecart"]["users"]
-    column.insert_one(form.dict())
+    user = models.FullUserData(form,
+                             _id=uuid.uuid4(),
+                             points=0,
+                             trips=0,
+                             hours=0.0)
+    user.change_password(user.password)
+    column.insert_one(user.dict())
     return models.register_form_output(success=True)
